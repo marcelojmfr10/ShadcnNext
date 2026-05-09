d6fae490c0c4f4679f33acbc82396675002b44f4
Marcelo Fuentes <marcelofuentes_10@hotmail.com>
2026-05-09 10:50:19 -0600

    Datatable mostrar datos, formato de celdas, acciones de fila y paginación

---
+++ app/dashboard/data-table/columns.tsx
@@ -0,0 +1,94 @@
+"use client";
+
+import { Badge } from "@/components/ui/badge";
+import { Payment } from "@/data/payments.data";
+import { ColumnDef } from "@tanstack/react-table";
+
+import { Button } from "@/components/ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { MoreHorizontal } from "lucide-react";
+import { toast } from "sonner";
+
+export const columns: ColumnDef<Payment>[] = [
+  {
+    accessorKey: "clientName",
+    header: "Client Name",
+  },
+  {
+    accessorKey: "status",
+    header: "Status",
+    cell: ({ row }) => {
+      const status = row.getValue("status") as string;
+      const variant =
+        {
+          pending: "secondary",
+          processing: "info",
+          success: "success",
+          failed: "destructive",
+        }[status] ?? ("default" as any);
+
+      return (
+        <Badge variant={variant} capitalize>
+          {status}
+        </Badge>
+      );
+    },
+  },
+  {
+    accessorKey: "amount",
+    header: () => <div className="text-right">Amount</div>,
+    cell: ({ row }) => {
+      const amount = parseFloat(row.getValue("amount"));
+      const formatted = new Intl.NumberFormat("en-US", {
+        style: "currency",
+        currency: "USD",
+      }).format(amount);
+
+      return <div className="text-right font-medium">{formatted}</div>;
+    },
+  },
+  {
+    accessorKey: "email",
+    header: "Email",
+  },
+  {
+    id: "actions",
+    cell: ({ row }) => {
+      const payment = row.original;
+
+      return (
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button variant="ghost" className="h-8 w-8 p-0">
+              <span className="sr-only">Open menu</span>
+              <MoreHorizontal className="h-4 w-4" />
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent align="end">
+            <DropdownMenuLabel>Actions</DropdownMenuLabel>
+            <DropdownMenuItem
+              onClick={() => {
+                navigator.clipboard.writeText(payment.id);
+                toast("Copied payment ID to clipboard", {
+                  position: "top-right",
+                });
+              }}
+            >
+              Copy payment ID
+            </DropdownMenuItem>
+            <DropdownMenuSeparator />
+            <DropdownMenuItem>View customer</DropdownMenuItem>
+            <DropdownMenuItem>View payment details</DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+      );
+    },
+  },
+];
